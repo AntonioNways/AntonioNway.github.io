ff169962d9a822cb53d1a2d580c1775d3f20e0d4
Silver Dn <Antonion90@gmail.com>
2017-07-29 01:23:25 -0400

    Remove mapping

--- components/App.jsx
+++ components/App.jsx
@@ -3,24 +3,6 @@ import { Link } from 'react-router';
 
 function App({ children, routes }) {
 
-  function generateMapMenu() {
-    let path = '';
-    return (
-      routes.filter(route => route.mapMenuTitle)
-        .map((route, index, array) => (
-          <span key={index}>
-            <Link
-              to={path += ((path.slice(-1) === '/' ? '' : '/') +
-                  (route.path === '/' ? '' : route.path))}
-            >
-              {route.mapMenuTitle}
-            </Link>
-            {(index + 1) < array.length && ' / '}
-          </span>
-        ))
-    );
-  }
-
   return(
     <div style={{maxWidth: '500px'}}>
       <h2 style={{marginBottom: 0}}>React for GitHub Pages</h2>
