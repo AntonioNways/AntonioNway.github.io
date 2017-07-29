1779f1edea7df0b2d034e54d88940a2b3de8ee58
Silver Dn <Antonion90@gmail.com>
2017-07-29 01:54:00 -0400

    fix bundle

--- components/App.jsx
+++ components/App.jsx
@@ -24,11 +24,6 @@ function App({ children, routes }) {
   return(
     <div style={{maxWidth: '500px'}}>
       <h2 style={{marginBottom: 0}}>React for GitHub Pages</h2>
-      <a href="https://github.com/rafrex/react-github-pages"
-        style={{marginBottom: '1em', display: 'block'}}
-      >
-        https://github.com/rafrex/react-github-pages
-      </a>
       {children}
     </div>
   );
