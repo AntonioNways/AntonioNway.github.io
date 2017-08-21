20db27a65ffb7600bfb49b72cec37ebd3f83c945
Silver Dn <Antonion90@gmail.com>
2017-08-21 15:19:04 -0400

    Update route to connect to project

--- components/ProjectPane.jsx
+++ components/ProjectPane.jsx
@@ -3,102 +3,15 @@ import { Link } from 'react-router';
 
 
 var ProjectPane = React.createClass({
-  componentDidMount(){
-    {this.renderSVG()}
+  renderAllList:function(val,x){  
+    return <div key={val} id={val} className="col-sm-12" ><div className="col-sm-4 projectPicPad" > <a href={this.props.route.ProjectData[val].link}><img alt={val} src={this.props.route.ProjectData[val].img} className="img-responsive"/></a></div>
+      <div className="col-sm-8"><h4>{val}</h4>{this.props.route.ProjectData[val].detail}</div>
+    </div>;
   },
-  renderSVG(){
-    var TimeLineData=[[2012,"B.B.A. Graduate with Distinction"],
-[2013,"Work as Account Payable"],[2014, "Promoted to Staff Accountant"],[2015,"Becomes CPA,CMA"], 
-[2016,"Explore Programming"],[2017.25,"March - Resign as an Accountant to pursue Programing"],[2017.43,"May - Obtained FCC's Front-end Development Certificate"],[2017.56,"July - Obtained FCC's Data-Visualization Certificate"]]
-    var xSpac=12;
-    var svg=d3.select("svg");
-    var g = svg.append("g");
-    var toolT=d3.select("#tooltip").style("opacity",0);
-    g.selectAll("circle")
-      .data(TimeLineData)
-      .enter()
-      .append("circle")
-      .style("opacity", 1)
-      .attr("class", "HoverTL")
-      .attr("cx",function(d,i){
-        return (d[0]-2011)*xSpac+25;
-      })
-      .attr("cy",function(d,i){
-        return (d[0]-2011)*55;
-      })
-      .attr("r", 5)
-      .attr("fill","blue")
-      .attr("stroke","brown")
-      .attr("stroke-width", "0.3")
-      //tooltip function here
-      .on("mouseover", function(d,i) {
-       toolT.transition()
-         .duration(300)
-         .style("opacity", .9);
-       toolT.html("<center>"+d[0].toString().substring(0,4)+" - "+d[1]+"</center>")
-         .style("left", Number(d3.select(this).attr("cx"))+55 + "px")
-         .style("top", Number(d3.select(this).attr("cy"))+60 + "px");
-       })
-     .on("mouseout", function(d) {
-       toolT.transition()
-         .duration(500)
-         .style("opacity", 0);
-       });
-  },
-  render : function() {
+  render: function () {
     return (
-    <div className="nav_pad container-fluid">
-      <div className="row">
-        <div className="col-md-12"><br></br></div>
-        <div className="col-md-1"></div>
-        <div className="col-md-10">
-        <div className="col-md-12">
-          <div className="col-md-8 HomePicStyle HomeALR">
-            <img src="http://via.placeholder.com/1000x300" className="img-responsive ThemeSize"/>
-          </div>
-          <div className="col-md-4 HomePicStyle">
-              <div className="col-md-12 HomePicStyle HomeARL"><Link to="/example"><img src="http://via.placeholder.com/290x97" className="img-responsive HomeAppSize"/></Link></div>
-              <div className="col-md-12 HomePicStyle HomeARL1"><Link to="/example"><img src="http://via.placeholder.com/290x97" className="img-responsive HomeAppSize"/></Link></div>
-              <div className="col-md-12 HomePicStyle HomeARL2"><Link to="/example"><img src="http://via.placeholder.com/290x97" className="img-responsive HomeAppSize"/></Link></div>
-          </div>
-        </div>
-        </div>
-        </div>
-          <div className="">
-      <div className="row">
-        <div className="col-md-12">
-          <div className="col-md-1"></div>
-          <div className="col-md-2 HomeALR1">
-            <br></br>
-            <br></br>
-            <br></br>
-            <img src="http://via.placeholder.com/290x200" className="img-responsive profileSize"/>
-            <h2>Antonio Ng</h2>  
-            <h4> Acountant (CPA, CMA) and Programmer</h4>
-          </div>
-          <div className="col-md-4 HomeABTqu">
-            <br></br>
-          <h1>About Me</h1>
-          
-                <br></br>
-                <p>In my 4 years of accounting experience, I had used my knowledge of Excel, SQL, and VBA to enhance the overall accounting system at work by automating multiple functions. From this experience, I became interested and started to learn programming. From there, I have learned Python, Django, HTML, and CSS. As of now, I am learning to program from Free Code Camp. </p>
-                <p>I am currently seeking for opportunities initiaives. Please contact me at Siiv3rd@gmail.com!</p>
-                <br></br>
-          </div>
-          <div className="col-md-4 HomeARL3">
-            <br></br>
-            <center><h3>History</h3></center>
-            <div className="tooltip" id="tooltip"></div>
-            <svg width="350" height="400">
-              <line x1={12+25} y1="55" x2={12*6.56+25} y2={55*6.56} stroke="black"/>
-            </svg>
-          </div>
-        </div>
-        </div>
-        <div className="col-md-1"></div>
-      </div>
-    </div>
-  );
+      <div>{Object.keys(this.props.route.ProjectData).map(this.renderAllList)}</div>
+    )
   }
 });
 
--- index.js
+++ index.js
@@ -10,6 +10,16 @@ import PageNotFound from './components/PageNotFound';
 import ExampleComponent from './components/ExampleComponent';
 import ExampleTwoDeepComponent from './components/ExampleTwoDeepComponent';
 
+///DATA to pass down to Children////
+var ProjectData= {"Dungeon Crawler":{"img":"https://drive.google.com/uc?export=download&id=0B9ldvGLcmpFzanZKbEJidURrM1E","link":"https://codepen.io/SilverAnt/full/PjVyLB/","detail": "A basic Dungeon Crawler game. Instructions: Travel the Dungeon an beat the boss in Dungeon level 4. (Note: Blue = Player, Red = enemy, Green = HP, Yellow = Weopen Upgrade, Purple = Move to next Dungeon)","type":"React" },
+"Game of Life":{"img":"https://drive.google.com/uc?export=download&id=0B9ldvGLcmpFzNUl0cE9JV2EyMzg","link":"https://codepen.io/SilverAnt/full/yXjEKx/","detail": "A simple game with where you can click on a cell to turn it alive or dead. After each generation, the cells will either reproduce or die. For more rules of the game, please refer to https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life","type":"React" }, 
+"Tic Tac Toe":{"img":"https://drive.google.com/uc?export=download&id=0B9ldvGLcmpFzOUM3ZnVvaXZQZDQ","link":"https://codepen.io/SilverAnt/full/EWOWvN/","detail": "A basic Tic Tac Toe game","type":"Other" },
+"Simon's Game":{"img":"https://drive.google.com/uc?export=download&id=0B9ldvGLcmpFzY2ZEWmh6YTN6MjQ","link":"https://codepen.io/SilverAnt/full/JNERyJ/","detail": "A basic game to test your memory. Try to repeat the pattern given by the computer","type":"Other" },
+"Force Directed Graph":{"img":"https://drive.google.com/uc?export=download&id=0B9ldvGLcmpFzMFFqREFfT2t1eFU","link":"https://codepen.io/SilverAnt/full/wqvKrz/","detail": "An interactive Force Directed Graph that connects countries by their borders. For fun, you can drag a country map around and view the effect on its neighbouring countries.","type":"D3" },
+"Visualize Data with a Scatterplot Graph":{"img":"https://drive.google.com/uc?export=download&id=0B9ldvGLcmpFzdk8tazBid00tMVE","link":"https://codepen.io/SilverAnt/full/qjzGPQ/","detail": "An interactive scatterplot graph that shows the doping allegations of professional bicycle racing","type":"D3" },
+"Calculator":{"img":"https://drive.google.com/uc?export=download&id=0B9ldvGLcmpFzcl9CWEt0enBtR1k","link":"https://codepen.io/SilverAnt/full/XMEREj/","detail": "A simple calculator App.","type":"Other" }
+}
+var allTabStat0={"allTabStat":["none","active","none","none"]}
 
 ///////////////////////////////////////////////////////////////////////////////
 // React for GitHub Pages - https://github.com/rafrex/react-github-pages
@@ -88,14 +98,8 @@ const routes = (
   <Route path="/" mapMenuTitle="Home" component={App} onEnter={checkForRedirect}>
     <IndexRoute component={Home} /> // pass the component as a child
 
+    <Route path="projects" mapMenuTitle="Projects" component={Projects} allTabStat={allTabStat0}/>
 
-    <Route path="project" mapMenuTitle="Projects component={Projects} allTabStat={{"allTabStat":["active","none","none","none"]}}>
-      <IndexRoute component={ProjectPane} />
-    </Route> 
-
-    <Route path="example" mapMenuTitle="Example" component={ExampleComponent}>
-      <Route path="two-deep" mapMenuTitle="Two Deep" component={ExampleTwoDeepComponent} />
-    </Route>
 
     <Route path="*" mapMenuTitle="Page Not Found" component={PageNotFound} />
   </Route>
