b92f658efc5901f1e50232b6e8da55077e8f91d5
Silver Dn <Antonion90@gmail.com>
2017-08-24 21:42:47 -0400

    Finish porfolio-resume sidebar

--- components/App.jsx
+++ components/App.jsx
@@ -84,10 +84,37 @@ var App = React.createClass({
          </div >
          <div id="resumebar" className={this.state.ProfState}>
            <div className="leftHomeNav container">
-             <div className="col-md-12">
+             <div className="col-md-12 RBBorder"><center>
               <img src="http://via.placeholder.com/290x200" className="img-responsive profileSize"/>
-              <Link to="/" className="">Antonio Ng&nbsp;</Link>
-             </div>
+              <Link to="/" className=""><h2>{this.state.AntInfo.basics.name}</h2></Link>
+              <h4>{this.state.AntInfo.basics.label}</h4>&nbsp;
+              </center></div>
+              
+            <div className="col-md-12">
+              <div className="Thr"></div>
+              <div>
+                <span className="glyphicon glyphicon-map-marker icon_sizeR" aria-hidden="true"></span>{this.state.AntInfo.basics.location}
+                <br></br>
+                <span className="glyphicon glyphicon-envelope icon_sizeR" aria-hidden="true"></span>{this.state.AntInfo.basics.email}
+                <br></br>
+                </div>
+                <div className="col-md-12">
+                <center>
+                    <a  href="https://ca.linkedin.com/in/antonio-ng-cpa-cma-7b8a6949" target="_blank">
+                      <img alt="Linkedin" src="http://www.freeiconspng.com/uploads/linkedin-icon-1.png" className="icon_sizeNP"/>
+                    </a>
+                    <a  href="https://github.com/AntonioNways" target="_blank">
+                      <img alt="GitHub" src="http://www.freeiconspng.com/uploads/github-logo-icon-5.png" className="icon_sizeNP"/>
+                    </a>
+                    <a  href="https://www.freecodecamp.com/antonionways" target="_blank">
+                      <img alt="FreeCodeCamp" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/231853/freecodecamp.png" className="icon_sizeNP"/>
+                    </a>
+                </center>
+              </div>
+              
+            </div>
+              
+             
            </div>
         </div>
         <div className={this.state.ChildPad}>
